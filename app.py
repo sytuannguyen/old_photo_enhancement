4bd6254d4abd9b342cdf2b97e7bc6727cb966c88
Sy-Tuan Nguyen <45685596+sytuannguyen@users.noreply.github.com>
2023-11-03 15:03:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,25 +9,24 @@ st.title("GFPGAN Environment Setup")
 if st.button("Clone GFPGAN Repository"):
     st.text("Cloning GFPGAN repository...")
     subprocess.run(["git", "clone", "https://github.com/TencentARC/GFPGAN.git"])
-    os.chdir("GFPGAN")
     st.success("GFPGAN repository cloned successfully!")
     st.write("Please proceed with the environment setup.")
 
 # Set up the environment
 if st.button("Set Up Environment"):
     st.text("Setting up the environment...")
-    subprocess.run(["pip", "install", "basicsr"])
-    subprocess.run(["pip", "install", "facexlib"])
-    subprocess.run(["pip", "install", "-r", "requirements.txt"])
-    subprocess.run(["python", "setup.py", "develop"])
-    subprocess.run(["pip", "install", "realesrgan"])
+    subprocess.run(["pip", "install", "GFPGAN/basicsr"])
+    subprocess.run(["pip", "install", "GFPGAN/facexlib"])
+    subprocess.run(["pip", "install", "-r", "GFPGAN/requirements.txt"])
+    subprocess.run(["python", "GFPGAN/setup.py", "GFPGAN/develop"])
+    subprocess.run(["pip", "install", "GFPGAN/realesrgan"])
     st.success("Environment set up successfully!")
 
 # Function to download pre-trained model
 def download_pretrained_model():
     st.text("Downloading pre-trained model...")
     model_url = "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth"
-    save_path = os.path.join("experiments", "pretrained_models", "GFPGANv1.3.pth")
+    save_path = os.path.join("GFPGAN/experiments", "pretrained_models", "GFPGANv1.3.pth")
     try:
         response = requests.get(model_url, stream=True)
         response.raise_for_status()
@@ -48,4 +47,3 @@ if st.button("Download Pre-trained Model"):
 if os.path.exists("experiments/pretrained_models/GFPGANv1.3.pth"):
     st.write("Pre-trained model is available and ready to use.")
 
-os.chdir("/mount/src/old_photo_enhancement")
